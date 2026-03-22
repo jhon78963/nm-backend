7c2d86673dbb6ab8d587d793e1091f38e00c1b2f
Jhon Livias <jhonlivias3@gmail.com>
2026-03-22 14:33:53 -0500

    register command

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,4 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withCommands([
+        \App\Inventory\Product\Commands\SyncZeroToMultikart::class,
+    ])
+    ->create();
