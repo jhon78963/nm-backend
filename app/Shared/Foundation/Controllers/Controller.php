6a939706214f4e8c8fe75fd533e8de2177983785
Jhon Livias <jhonlivias3@gmail.com>
2026-06-20 17:11:18 -0500

    fix

--- app/Shared/Foundation/Controllers/Controller.php
+++ app/Shared/Foundation/Controllers/Controller.php
@@ -3,11 +3,13 @@
 namespace App\Shared\Foundation\Controllers;
 
 use App\Shared\Foundation\Exceptions\ApiExceptionRenderer;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\JsonResponse;
 use Throwable;
 
 abstract class Controller
 {
+    use AuthorizesRequests;
     protected function apiErrorResponse(
         Throwable $exception,
         int $status,
