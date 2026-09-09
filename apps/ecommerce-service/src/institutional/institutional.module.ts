391c912ec4e8118b3bcc0600aef59116d305368c
Jhon Livias <jhonlivias3@gmail.com>
2026-09-09 15:26:02 -0500

    Fix ecommerce institutional module missing RecaptchaModule import.
    InstitutionalController injects RecaptchaService but the module did not import RecaptchaModule, causing ecommerce-service to crash on boot.

--- apps/ecommerce-service/src/institutional/institutional.module.ts
+++ apps/ecommerce-service/src/institutional/institutional.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { RecaptchaModule } from '@app/common/recaptcha/recaptcha.module';
 import { MailClientModule } from '@app/mail-client';
 
 import { InstitutionalController } from './institutional.controller';
 import { InstitutionalService } from './institutional.service';
 
 @Module({
-  imports: [MailClientModule],
+  imports: [RecaptchaModule, MailClientModule],
   controllers: [InstitutionalController],
   providers: [InstitutionalService],
 })
